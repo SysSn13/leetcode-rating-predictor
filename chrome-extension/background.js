bfbb395e86f0dcc63b06f876c199394fd5209bf6
SysSn13 <sudesh18@iitg.ac.in>
2021-08-06 04:01:11 +0530

    fix: instead of click listener listen to url changes to handle all the cases

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,40 +1,122 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (
         changeInfo.status === "complete" &&
-        /^https:\/\/leetcode.com\/contest\/.*\/ranking\//.test(tab.url)
+        /^https:\/\/leetcode.com\/contest\/.+/.test(tab.url)
     ) {
         chrome.scripting
             .executeScript({
-                target: { tabId: tabId },
+                target: {
+                    tabId: tabId,
+                },
                 files: ["./foreground.js"],
             })
             .then(() => {
                 console.log(
                     `Injected the foreground script into tab: ${tabId}`
                 );
+                chrome.tabs.sendMessage(tabId, {
+                    message: "url_updated",
+                    url: tab.url,
+                });
             })
             .catch((err) => console.error(err));
     }
 });
 
-const BASE_URL = new URL("http://127.0.0.1:8080/api/v1/predictions"); // TODO: replace it
+const API_URLS = [
+    "https://leetcode-rating-predictor.herokuapp.com/api/v1/predictions",
+    "https://leetcode-predictor.herokuapp.com/api/v1/predictions",
+];
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === "get_predictions") {
-        const url = BASE_URL;
-        url.searchParams.set("contestId", request.data.contestId);
-        let handles = "";
-        request.data.handles.forEach((handle, index) => {
-            handles +=
-                handle + (index !== request.data.handles.length - 1 ? ";" : "");
-        });
-        url.searchParams.set("handles", handles);
-        fetch(url)
-            .then((res) => res.json())
+        getPredictions(request.data)
             .then((res) => {
+                // console.log(res);
                 sendResponse(res);
             })
-            .catch((err) => console.error(err));
+            .catch((err) => {
+                console.error(err);
+            });
         return true;
     }
 });
+
+async function getPredictions(data) {
+    try {
+        let urlIndex = await getURL_INDEX();
+        if (urlIndex === undefined || urlIndex < 0 || urlIndex >= API_URLS) {
+            urlIndex = 0;
+        }
+        for (let i = 0; i < API_URLS.length; i++) {
+            try {
+                const ind = (i + urlIndex) % API_URLS.length;
+                const url = new URL(API_URLS[ind]);
+
+                url.searchParams.set("contestId", data.contestId);
+                let handles = "";
+                data.handles.forEach((handle, index) => {
+                    handles +=
+                        handle + (index !== data.handles.length - 1 ? ";" : "");
+                });
+                url.searchParams.set("handles", handles);
+
+                const resp = await fetchFromAPI(url);
+                setURL_INDEX(ind).catch((err) => {
+                    console.error(err);
+                });
+                return resp;
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function fetchFromAPI(url, retries = 5) {
+    let resp = await fetch(url);
+    if (resp.status !== 200) {
+        if (retries > 0) {
+            resp = await fetchFromAPI(url, retries - 1);
+            return resp;
+        }
+        throw new Error(resp.statusText);
+    }
+    resp = await resp.json();
+    return resp;
+}
+
+async function setURL_INDEX(index) {
+    try {
+        const promise = new Promise((resolve, reject) => {
+            chrome.storage.sync.set(
+                {
+                    url_index: index,
+                },
+                function () {
+                    resolve();
+                }
+            );
+        });
+        await promise;
+    } catch (err) {
+        return err;
+    }
+}
+
+async function getURL_INDEX() {
+    try {
+        const promise = new Promise((resolve, reject) => {
+            chrome.storage.sync.get(["url_index"], function (result) {
+                resolve(result.url_index);
+            });
+        });
+        const index = await promise;
+        return index;
+    } catch (err) {
+        console.error(err);
+        return -1;
+    }
+}
--- chrome-extension/foreground.js
+++ chrome-extension/foreground.js
@@ -7,8 +7,13 @@ if (!window.CFPredictorInjected) {
             const tbody = document.querySelector("tbody");
             if (!tbody) {
                 setTimeout(setEventListener, 100);
+                return;
             }
             const trs = tbody.querySelectorAll("tr");
+            if (!trs) {
+                setTimeout(setEventListener, 100);
+                return;
+            }
             if (trs.length <= 1) {
                 // listen only if there is more than one row
                 isListenerActive = false;
@@ -19,33 +24,54 @@ if (!window.CFPredictorInjected) {
                 isListenerActive = false;
                 return;
             }
+            if (isListenerActive) return;
             isListenerActive = true;
-            tds[1].addEventListener("DOMCharacterDataModified", () => {
+            tds[1].addEventListener("DOMCharacterDataModified", async () => {
                 window.clearTimeout(predictionsTimer);
-                predictionsTimer = setTimeout(fetchPredictions, 1000);
+                predictionsTimer = setTimeout(fetchPredictions, 500);
             });
         } catch (err) {
             console.error(err);
         }
     };
 
+    const isContestRankingPage = (url) => {
+        return /^https:\/\/leetcode.com\/contest\/.*\/ranking/.test(url);
+    };
+
     let rowsChanged = new Map();
-    let colInserted = false;
+    let deltaTHInserted = false;
 
     const fetchPredictions = async () => {
         const thead = document.querySelector("thead");
         if (!thead) {
-            predictionsTimer = setTimeout(fetchPredictions, 100);
+            predictionsTimer = setTimeout(fetchPredictions, 500);
             return;
         }
+
         const tbody = document.querySelector("tbody");
+        if (!tbody) {
+            predictionsTimer = setTimeout(fetchPredictions, 500);
+            return;
+        }
+
         const rows = tbody.querySelectorAll("tr");
-        const contestId = document
-            .querySelector(".ranking-title-wrapper")
-            .querySelector("span")
-            .querySelector("a")
-            .innerHTML.toLowerCase()
-            .replace(/\s/g, "-");
+        if (!rows) {
+            predictionsTimer = setTimeout(fetchPredictions, 500);
+            return;
+        }
+        let contestId;
+        try {
+            contestId = document
+                .querySelector(".ranking-title-wrapper")
+                .querySelector("span")
+                .querySelector("a")
+                .innerHTML.toLowerCase()
+                .replace(/\s/g, "-");
+        } catch {
+            predictionsTimer = setTimeout(fetchPredictions, 500);
+            return;
+        }
         const handlesMap = new Map();
 
         const handles = [...rows].map((row, index) => {
@@ -70,90 +96,114 @@ if (!window.CFPredictorInjected) {
                     return handle;
                 }
             } catch (err) {
-                console.error(err);
+                console.debug(err);
             }
         });
 
         chrome.runtime.sendMessage(
             {
                 message: "get_predictions",
-                data: { contestId, handles },
+                data: {
+                    contestId,
+                    handles,
+                },
             },
             (response) => {
-                if (response.status === "OK") {
-                    if (!colInserted) {
-                        const th = document.createElement("th");
-                        th.innerText = "Δ";
-                        thead.querySelector("tr").appendChild(th);
-                        colInserted = true;
-                    }
-                    const rowsUpdated = new Map();
-                    for (item of response.items) {
-                        try {
-                            const id = (
-                                item.data_region +
-                                "/" +
-                                item._id
-                            ).toLowerCase();
-                            if (handlesMap.has(id)) {
-                                const rowIndex = handlesMap.get(id);
-                                const row = rows[rowIndex];
-                                let td;
-                                if (rowsChanged.has(rowIndex)) {
-                                    td = row.lastChild;
-                                } else {
-                                    td = document.createElement("td");
-                                }
-                                if (item.delta == null) {
-                                    td.innerText = "?";
-                                    td.style.color = "gray";
-                                } else {
-                                    const delta =
-                                        Math.round(item.delta * 100) / 100;
-                                    td.innerText =
-                                        delta > 0 ? "+" + delta : delta;
-                                    if (delta > 0) {
-                                        td.style.color = "green";
-                                    } else {
-                                        td.style.color = "gray";
-                                    }
-                                    // td.style.fontWeight = "bold";
-                                }
-                                if (!rowsChanged.has(rowIndex)) {
-                                    row.appendChild(td);
-                                }
-                                rowsUpdated.set(rowIndex, true);
-                            }
-                        } catch (err) {
-                            console.error(err);
+                if (!response) {
+                    return;
+                }
+                try {
+                    if (response.status === "OK") {
+                        if (!deltaTHInserted && response.meta.total_count) {
+                            const th = document.createElement("th");
+                            th.innerText = "Δ";
+                            thead.querySelector("tr").appendChild(th);
+                            deltaTHInserted = true;
                         }
-                    }
-                    for (rowIndex of rowsChanged.keys()) {
-                        if (!rowsUpdated.has(rowIndex)) {
+                        const rowsUpdated = new Map();
+                        for (item of response.items) {
                             try {
-                                const row = rows[rowIndex];
-                                row.lastChild.innerText = "";
+                                const id = (
+                                    item.data_region +
+                                    "/" +
+                                    item._id
+                                ).toLowerCase();
+                                if (handlesMap.has(id)) {
+                                    const rowIndex = handlesMap.get(id);
+                                    const row = rows[rowIndex];
+                                    let td;
+                                    if (rowsChanged.has(rowIndex)) {
+                                        td = row.lastChild;
+                                    } else {
+                                        td = document.createElement("td");
+                                    }
+                                    if (item.delta == null) {
+                                        td.innerText = "?";
+                                        td.style.color = "gray";
+                                    } else {
+                                        const delta =
+                                            Math.round(item.delta * 100) / 100;
+                                        td.innerText =
+                                            delta > 0 ? "+" + delta : delta;
+                                        if (delta > 0) {
+                                            td.style.color = "green";
+                                        } else {
+                                            td.style.color = "gray";
+                                        }
+                                        // td.style.fontWeight = "bold";
+                                    }
+                                    if (!rowsChanged.has(rowIndex)) {
+                                        row.appendChild(td);
+                                    }
+                                    rowsUpdated.set(rowIndex, true);
+                                }
                             } catch (err) {
-                                console.error(err);
+                                console.warn(err);
                             }
                         }
+                        for (rowIndex of rowsChanged.keys()) {
+                            if (
+                                !rowsUpdated.has(rowIndex) &&
+                                rowIndex < rows.length
+                            ) {
+                                try {
+                                    const row = rows[rowIndex];
+                                    row.lastChild.innerText = "";
+                                } catch {}
+                            }
+                            if (rowIndex >= rows.length) {
+                                rowsChanged.delete(rowIndex);
+                            }
+                        }
+                        for (rowIndex of rowsUpdated.keys()) {
+                            rowsChanged.set(rowIndex, true);
+                        }
                     }
-                    for (rowIndex of rowsUpdated.keys()) {
-                        rowsChanged.set(rowIndex, true);
-                    }
+                } catch (err) {
+                    console.warn(err);
                 }
             }
         );
     };
-    fetchPredictions();
-    setEventListener();
 
-    // event listener for "click" event on page-btn class items
-    [...document.querySelectorAll(".page-btn")].forEach((item) => {
-        item.addEventListener("click", (e) => {
-            if (!isListenerActive) {
+    // listen to url changes
+    chrome.runtime.onMessage.addListener(function (
+        request,
+        sender,
+        sendResponse
+    ) {
+        if (request.message === "url_updated") {
+            if (!isListenerActive && isContestRankingPage(request.url)) {
+                window.clearTimeout(predictionsTimer);
+                predictionsTimer = setTimeout(fetchPredictions, 500);
+            }
+            if (!isContestRankingPage(request.url)) {
+                isListenerActive = false;
+                rowsChanged.clear();
+                deltaTHInserted = false;
+            } else {
                 setEventListener();
             }
-        });
+        }
     });
 }
